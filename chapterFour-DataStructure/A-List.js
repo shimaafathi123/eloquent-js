884339712200187522cfe7f6469284975f575fdf
shimaaFathi <shimaafathi126@gmail.com>
2024-05-06 10:30:12 +0300

    arrayToList

---
+++ chapterFour-DataStructure/A-List.js
@@ -0,0 +1,10 @@
+//arrayToList Function
+function arrayToList(arr) {
+    let list = null;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = {value: arr[i], rest: list};
+    }
+    return list;
+}
+
+console.log(arrayToList([1, 5, 8]));
