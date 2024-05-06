084161314faa520871d24d891884fc6f5a458115
shimaaFathi <shimaafathi126@gmail.com>
2024-05-06 10:34:09 +0300

    listToArray

--- chapterFour-DataStructure/A-List.js
+++ chapterFour-DataStructure/A-List.js
@@ -6,5 +6,16 @@ function arrayToList(arr) {
     }
     return list;
 }
-
 console.log(arrayToList([1, 5, 8]));
+
+//listToArray Function
+function listToArray(list) {
+    let arr = [];
+    while (list !== null) {
+        arr.push(list.value);
+        list = list.rest;
+    }
+    return arr;
+}
+console.log(listToArray({ value: 1, rest: { value: 5, rest: { value: 8, rest: null } } }));
+  
